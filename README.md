6c4bf13102fc6b0dd6e62ea5ba5243bfe79ba8b8
Terence Ramos <terence.ramos@zerve.com>
2015-04-28 21:09:32 -0700

    readme update and comments.

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # angularjs-rating-view
 Rating icon widget using Font Awesome.
 
+This will display rating icons according to the rating value using a font
+awesome icon. Ergo, font awesome is required for this to work.
+
+```
+<div rating="1" max="4" step="0.1"></div>
+```
+
+* **rating** - The rating value to represent.
+
+* **max** - The max rating possible.
+
+* **step** - The minimum step increment that will be represented. For example, if this is set to 0.25 and the rating value is 0.45, it will round to 0.5.
--- js/rating-widget.js
+++ js/rating-widget.js
@@ -12,37 +12,48 @@
                 link: function(scope, element, attrs) {
 
                     scope.max = scope.max || 4;
-
-                    if (!angular.isDefined(scope.max)) {
-                        scope.max = 4;
-                    }
-
                     scope.icon = scope.icon || 'star';
-                    var scope.icon = 'star';
-                    if (angular.isDefined(attrs.icon)) {
-                        scope.icon = attrs.icon;
-                    }
 
+                    // Create the icon template.
                     var icon = '<i class="fa fa-' + scope.icon + '"></i>';
 
+                    // Set the needed css styles.
                     var e = angular.element(element);
-                    e.addClass('background');
                     e.css({
                         'display': 'inline-block',
                         'position': 'relative'
                     });
+
+                    // Local variable for element height that will be used by
+                    // other functions.
                     var h = 0;
 
+                    // Initializer to generate the number of icons needed to
+                    // represent this rating.
                     var initialize = function() {
+
+                        // Clear the current children first.
                         e.children().remove();
+
+                        // Generate the number of icons needed.
                         var temp = '';
                         for (var n = 0; n < scope.max; n++) {
                             temp += icon;
                         }
                         e.append(temp);
+
+                        // Generate the containing div.
                         var div = angular.element('<div class="foreground"></div>');
+
+                        // Append the rating icons.
                         div.append(temp);
+
+                        // Set the height after having set the icons so that we
+                        // get the actual height.
                         h = element[0].clientHeight;
+
+                        // Set the initial css for the div that will display the
+                        // rating icons according to the rating.
                         div.css({
                             'position': 'absolute',
                             'width': 0 + 'px',
@@ -56,22 +67,28 @@
 
                     initialize();
 
+                    // Update the icons according to ther ating.
                     var update = function() {
+
+                        // This needs to be done in an interval so that the
+                        // DOM elements have time to be set properly.
                         $interval(function() {
+                            // Calculate the rating value according to the step.
                             var rating = Math.round(scope.rating / scope.step) * scope.step;
-                            console.log('rating = ' + rating);
 
+                            // Calculate the rating width the icons will
+                            // represent.
                             var w = element[0].clientWidth;
-                            console.log('w = ' + w);
                             var ow = (parseFloat(rating) / scope.max) * w;
-                            console.log('ow = ' + ow);
 
+                            // Set the width for foreground icons.
                             angular.element(element).find('div').css({
                                 'width': ow + 'px'
                             });
                         }, 100, 1);
                     };
 
+                    // Set watches for max, rating, and step values.
                     scope.$watch(function(scope) {
                         return scope.max;
                     }, function() {
